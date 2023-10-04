cb8c90abf36daf2ab6db67788bb4f0937bab1658
Gorka Puente Díez <gorkapuentediez@gmail.com>
2023-10-04 17:15:07 +0200

    Added javadoc for GameplayManager, PlayerLoginListener and TeamManager
    gorkiiuss - 04/10/2023

--- src/main/java/com/github/gorkiiuss/uhcpantuflas/player/PlayerLoginListener.java
+++ src/main/java/com/github/gorkiiuss/uhcpantuflas/player/PlayerLoginListener.java
@@ -7,8 +7,20 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 
+/**
+ * The PlayerLoginListener class is responsible for handling player login events.
+ *
+ * @version 0.0.1-ALPHA.0
+ * @since 04/10/2023-ALPHA.0
+ */
 public class PlayerLoginListener implements Listener {
     private final String PLAYER_NO_TEAM_KICK_MSG = "You have to be on a team to participate!";
+
+    /**
+     * Handles player login events and prevents players from logging in if they are not on a team in the MIN_ZERO_TEAMS game mode.
+     *
+     * @param event The PlayerLoginEvent to handle.
+     */
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent event) {
         if (GameplayManager.get().getGameMode() == UHCGameMode.MIN_ZERO_TEAMS) {
