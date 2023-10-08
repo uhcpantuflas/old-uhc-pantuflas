b7db6f2d035859f1402a52e88f1fc10308d94bfd
Gorka Puente Díez <gorkapuentediez@gmail.com>
2023-10-08 20:58:36 +0200

    Added documentation for game state and players interface gorkiiuss - 08/10/2023

--- src/main/java/com/github/gorkiiuss/uhcpantuflas/gameplay/GameState.java
+++ src/main/java/com/github/gorkiiuss/uhcpantuflas/gameplay/GameState.java
@@ -1,8 +1,26 @@
 package com.github.gorkiiuss.uhcpantuflas.gameplay;
 
+/**
+ * The GameState enum represents different states of a UHC (Ultra Hardcore) game.
+ * It is used to track the current phase of the game, such as the beginning, middle, or end.
+ *
+ * @version 0.0.1-ALPHA.0
+ * @since 08/10/2023
+ */
 public enum GameState {
+    /**
+     * Represents the beginning state of a UHC game.
+     * This is typically the initial state when players are preparing for the game.
+     */
     BEGINNING;
 
+    /**
+     * Gets the GameState corresponding to the provided game state index.
+     *
+     * @param gameStateIdx The index of the game state.
+     * @return The GameState enum constant corresponding to the provided index.
+     * @throws ArrayIndexOutOfBoundsException if the index is out of bounds.
+     */
     public static GameState get(int gameStateIdx) {
         return values()[gameStateIdx];
     }
--- src/main/java/com/github/gorkiiuss/uhcpantuflas/player/PlayerManager.java
+++ src/main/java/com/github/gorkiiuss/uhcpantuflas/player/PlayerManager.java
@@ -2,23 +2,46 @@ package com.github.gorkiiuss.uhcpantuflas.player;
 
 import java.util.ArrayList;
 
+/**
+ * The PlayerManager class is responsible for managing UHC (Ultra Hardcore) players in the plugin.
+ * It provides methods to register players and check if a player is registered.
+ *
+ * @version 0.0.1-ALPHA.0
+ * @since 08/10/2023
+ */
 public class PlayerManager {
     private static PlayerManager instance;
     private final ArrayList<UHCPlayer> players = new ArrayList<>();
 
     private PlayerManager() {
-
+        // Private constructor to enforce singleton pattern
     }
 
+    /**
+     * Get the instance of the PlayerManager.
+     *
+     * @return The PlayerManager instance.
+     */
     public static synchronized PlayerManager get() {
         if (instance == null) instance = new PlayerManager();
         return instance;
     }
 
+    /**
+     * Register a UHC player.
+     *
+     * @param player The UHCPlayer to register.
+     */
     public void registerPlayer(UHCPlayer player) {
         players.add(player);
     }
 
+    /**
+     * Check if a player is registered.
+     *
+     * @param playerName The name of the player to check.
+     * @return True if the player is registered, otherwise false.
+     */
     public boolean isPlayerRegistered(String playerName) {
         return players.stream().anyMatch(uhcPlayer -> uhcPlayer.hasName(playerName));
     }
